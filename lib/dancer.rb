d154715b8c1a4dd6da408af75852ac0ae99c8702
Mitzi Estrada <m_estrada17@live.com>
2020-03-13 05:01:28 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -8,8 +8,10 @@ class Dancer
   # extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  attr_accessor :name
 
-  #def initialize(name)
-    #@name = name
-  #end
+  def initialize(name)
+    @name = name
+  end
 end
